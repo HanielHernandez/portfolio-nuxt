fba16e9f5c1660d76694072025d3958085bebdee
Haniel Hernández <haniel1121@outlook.com>
2025-03-15 09:40:24 -0600

    Updated links looks

--- constants/constants.ts
+++ constants/constants.ts
@@ -3,11 +3,12 @@ import { BLOCKS, INLINES } from '@contentful/rich-text-types'
 
 export const RICH_TEXT_OPTIONS: Partial<Options> = {
     renderNode: {
-        [BLOCKS.PARAGRAPH]: (node: any, next: (nodes: any) => string) => `<p class="mb-4">${next(node.content)}</p>`,
+        [BLOCKS.PARAGRAPH]: (node: any, next: (nodes: any) => string) =>
+            `<p class="mb-4 text-neutral-600">${next(node.content)}</p>`,
         [BLOCKS.UL_LIST]: (node: any, next: (nodes: any) => string) =>
             `<ul class="list-disc mb-4 pl-4">${next(node.content)}</ul>`,
         [BLOCKS.LIST_ITEM]: (node: any, next: (nodes: any) => string) => `<li>${next(node.content)}</li>`,
         [INLINES.HYPERLINK]: (node: any, next: (nodes: any) => string) =>
-            `<a class="text-blue-600 hover:text-blue-800">${next(node.content)}</a>`
+            `<a class="text-blue-600 hover:opacity-75 font-medium cursor-pointer">${next(node.content)}</a>`
     }
 }
